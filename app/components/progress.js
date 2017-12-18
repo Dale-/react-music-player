2f77bdaeb30b993fbaea28402998ccae60c0c880
Dale- <18717394542@163.com>
2017-12-18 23:06:48 +0800

    modify the css name to components

--- app/components/progress.js
+++ app/components/progress.js
@@ -4,7 +4,7 @@ import './progress.less'
 let Progress = React.createClass({
     render() {
         return (
-        	<div className="component-progress">
+        	<div className="components-progress">
 	            <div className="progress" style={{width: `${this.props.progress}%`}}></div>
         	</div>
         );
