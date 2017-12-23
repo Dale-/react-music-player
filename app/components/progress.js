02afefbac1609110d64a59247358cd6e33e16c5a
Dale- <18717394542@163.com>
2017-12-23 17:25:40 +0800

    add changeProgess method for change the song's progress

--- app/components/progress.js
+++ app/components/progress.js
@@ -2,13 +2,18 @@ import React from 'react';
 import './progress.less'
 
 let Progress = React.createClass({
-    render() {
-        return (
-        	<div className="components-progress">
-	            <div className="progress" style={{width: `${this.props.progress}%`}}></div>
-        	</div>
-        );
-    }
+  changeProgess(e) {
+    let progressBar = this.refs.progressBar;
+    let progress = (e.clientX - progressBar.getBoundingClientRect().left) / progressBar.clientWidth;
+    console.log(progress);
+  },
+  render() {
+      return (
+      	<div className="components-progress" ref="progressBar" onClick={this.changeProgess}>
+            <div className="progress" style={{width: `${this.props.progress}%`}}></div>
+      	</div>
+      );
+  }
 });
 
 export default Progress;
