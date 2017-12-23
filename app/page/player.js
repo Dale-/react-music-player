03727b5961343a55603c5e5bf17d4e4d476642a3
Dale- <18717394542@163.com>
2017-12-23 18:52:53 +0800

    split root to player js

---
+++ app/page/player.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import Progress from '../components/progress'
+
+let duration = null;
+let Player = React.createClass({
+
+  getInitialState() {
+    return {
+      progress: 0
+    }
+  },
+
+  componentDidMount() {
+		$("#player").bind($.jPlayer.event.timeupdate, (e) => {
+      duration = e.jPlayer.status.duration;
+			this.setState({
+				progress: e.jPlayer.status.currentPercentAbsolute
+			});
+		});
+  },
+
+  componentWillUnmout() {
+    $("#player").unbind($.jPlayer.event.timeupdate);
+  },
+
+  handleProgressChange(progress) {
+    $("#player").jPlayer('play', duration * progress);
+  },
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Progress
+          progress={this.state.progress}
+          onProgressChange={this.handleProgressChange}
+          barColor="#ffee00"
+        >
+        </Progress>
+      </div>
+    );
+  }
+
+});
+
+export default Player;
--- app/root.js
+++ app/root.js
@@ -1,40 +1,7 @@
 import React from 'react'
-import Header from './components/header'
 import Progress from './components/progress'
 
-let duration = null;
 let Root = React.createClass({
-
-  componentDidMount() {
-    $("#player").jPlayer({
-			ready: function () {
-				$(this).jPlayer("setMedia", {
-					mp3: "http://oj4t8z2d5.bkt.clouddn.com/%E9%AD%94%E9%AC%BC%E4%B8%AD%E7%9A%84%E5%A4%A9%E4%BD%BF.mp3"
-				}).jPlayer('play');
-			},
-			supplied: "mp3",
-			wmode: "window",
-			useStateClassSkin: true
-		});
-		$("#player").bind($.jPlayer.event.timeupdate, (e) => {
-      duration = e.jPlayer.status.duration;
-			this.setState({
-				progress: e.jPlayer.status.currentPercentAbsolute
-			});
-		});
-  },
-
-  componentWillUnmout() {
-    $("#player").unbind($.jPlayer.event.timeupdate);
-  },
-  getInitialState() {
-    return {
-      progress: 0
-    }
-  },
-  handleProgressChange(progress) {
-    $("#player").jPlayer('play', duration * progress);
-  },
   render() {
     return (
       <div>
